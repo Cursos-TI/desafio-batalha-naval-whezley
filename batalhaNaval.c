ffae0daa1e09ead843258549f6d7fb986677a0c4
whezley <francisco.w.m.q@gmail.com>
2025-08-22 18:54:44 +0000

    Implementa tabuleiro e funcoes pra adicionar os  navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,37 +4,94 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    int tamanhoTabuleiro = 9;
-    const int colunas = 10;
-    const int linhas = 10;
-    int tabuleiro[linhas][colunas] = {
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0}
 
-    };
+void apresentacao (int linhas, int colunas) {
+    // Função para exibir a apresentação do jogo
+    printf("Bem-vindo ao jogo Batalha Naval!\n");
+    printf("Tabuleiro de %d linhas por %d colunas\n", linhas, colunas);
+}
 
-    //Cria um laco de repeticao para exibir a matriz
-    for (int i = 0; i <= linhas; i++){
-        for (int j = 0; j <= colunas; j++){
-            printf(" %d ", tabuleiro[i][j]);
-        
+void exibirTabuleiro(int tabuleiro[10][10], int posicaonavio1[2],int posicaonavio2[2]) {
+   
+    // Função para exibir o tabuleiro
+    printf("Tabuleiro:\n");
+    for (int i = 0; i <= 9; i++){
+        for (int j = 0; j <= 9; j++){
+            tabuleiro[i][j] = 0; // Inicializa todas as posições com 0
+            adicionarNavioHorizontal(i, j, tabuleiro, posicaonavio1);
+            adicionarNavioVertical(i, j, tabuleiro, posicaonavio2);
+
+           printf("%d ", tabuleiro[i][j]);
+            
         }
         printf("\n");
 
     }
-     
-     
+}
+
+
+
+void adicionarNavioHorizontal(int i,int j,int tabuleiro[10][10], int posicaonavio1[2]) {
+    // Função para adicionar um navio ao tabuleiro
+if (i == posicaonavio1[0] && j == posicaonavio1[1]) {
+                tabuleiro[i][j] = 3; // Marca a posição do navio com 3
+               // tabuleiro[i][j+1] = 3;
+            }
+            if (i == posicaonavio1[0] && j == posicaonavio1[1]+1) {
+                tabuleiro[i][j] = 3; // Marca a posição do navio com 3
+                //tabuleiro[i][j+1] = 3;
+            }
+            if (i == posicaonavio1[0] && j == posicaonavio1[1]+2) {
+                tabuleiro[i][j] = 3; // Marca a posição do navio com 3
+                //tabuleiro[i][j+1] = 3;
+            }
+
+}
+
+void adicionarNavioVertical(int i,int j,int tabuleiro[10][10], int posicaonavio1[2]) {
+    // Função para adicionar um navio ao tabuleiro
+if (i == posicaonavio1[0] && j == posicaonavio1[1]) {
+                tabuleiro[i][j] = 3; // Marca a posição do navio com 3
+                
+            }
+            if (i == posicaonavio1[0]+1 && j == posicaonavio1[1]) {
+                tabuleiro[i][j] = 3; // Marca a posição do navio com 3
+            }
+            if (i == posicaonavio1[0]+2 && j == posicaonavio1[1]) {
+                tabuleiro[i][j] = 3; // Marca a posição do navio com 3
+            }
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    // Nível Novato - Posicionamento dos Navios
+    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+    const int colunas = 10;
+    const int linhas = 10;
+    int tabuleiro[linhas][colunas];
+    char posicionenalinha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    char posicionenacoluna [10] = { '0','1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int posicaonavio1[2] = {1,2}; // Posição do primeiro navio (linha, coluna)
+    int posicaonavio2[2] = {3,3}; // Posição do segundo navio (linha, coluna)
+    //int posicaonavio2[3][1] = {{3},{3},{3}}; // Posição do segundo navio (linha, coluna)
+    //Cria um laco de repeticao para exibir a matriz
+
+    apresentacao(linhas, colunas);
+    exibirTabuleiro(tabuleiro,posicaonavio1, posicaonavio2);
 
 
 
